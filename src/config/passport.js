aadcb8ae9ead8c371e036b51d0aedb5118e3575e
Siddharth Bhardwaj <234882308+siddharth-xzayogn@users.noreply.github.com>
2025-10-04 21:30:48 +0530

    CI YML File

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: (process.env.BACKEND_URL || 'https://trueport-backend.onrender.com') + '/api/auth/google/callback'
+    callbackURL: (process.env.BACKEND_URL || 'https://trueport-backend.onrender.com') + '/auth/google/callback'
 
 }, async (accessToken, refreshToken, profile, done) => {
     try {
